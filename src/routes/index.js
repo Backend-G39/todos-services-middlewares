6e05cb168f1af60ceb9bdd954c85240aef68deaa
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-10-02 21:37:21 -0300

    advances

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routerUser = require('./router/user.router');
+const { verifyJWT } = require('../utils/verifyJWT');
+const routerToDo = require('./router/toDo.router');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use('/users', routerUser)
+router.use('/todos', verifyJWT, routerToDo)
 
 
 module.exports = router;
---
+++ src/routes/router/toDo.router.js
@@ -0,0 +1,15 @@
+const { getAll, create, getOne, remove, update } = require('../../controllers/todo.controllers');
+const express = require('express');
+
+const routerToDo = express.Router();
+
+routerToDo.route('/')
+  .get(getAll) //🔐👌
+  .post(create);  //🔐👌
+
+routerToDo.route('/:id')
+  .get(getOne)  //🔐👌
+  .delete(remove)  //🔐👌
+  .put(update);  //🔐👌
+
+module.exports = routerToDo;
